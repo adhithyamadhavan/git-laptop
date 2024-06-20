33cff13ab826de367a24a766095341de36a3e7ea
adhithyamadhavan <adhithyamadhavan2002@gmail.com>
2024-06-20 12:39:57 +0530

    just for fun

---
+++ hello world.py
@@ -0,0 +1,20 @@
+"""
+
+name = "adhithya"
+last_name = "madhavan"
+age = 21
+is_Male = False
+
+print(name.replace("a","b"))
+print("age is %(age)d" % {"age": age}) // this is stupid use str(a).
+
+
+name = "WoLFie"
+age = 69
+
+print("name is " + name)
+print("age is %(age)d" % {"age": age})
+
+"""
+a=10
+print(max(563,5))
